992fb728c9cfa72b7d0bb01f5282241b96b79f45
Liz <51245710+eahowell@users.noreply.github.com>
2024-05-27 18:49:31 -0500

    Fixed errors on API properties

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,55 +3,71 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view.jsx";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([]);
-  
+  const [Movies, setMovies] = useState([]);
+
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
     fetch("https://myflix-eahowell-7d843bf0554c.herokuapp.com/movies/")
-    .then((response) => response.json())
-    .then((data) => {
-      const moviesFromAPI = data.docs.map((doc) =>{
-        return {
-          _id: doc._id,
-          Title: doc.Title,
-          Director: {
-              Name: doc. Director.Name,
-              Birthday: doc. Director.Birthday,
-              Deathday: doc. Director.Deathday,
-              Bio: doc. Director.Bio 
-            },
-          Genre: {
-            Name: doc.Genre.Name,
-            Description: doc.Genre.Description 
-          },
-          Actors: doc.Actors,
-          Description: doc.Description,
-          ImagePath: doc.ImagePath
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch movies.");
         }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        const moviesFromAPI = data.map((Movie) => {
+          let directorDeathday = Movie.Director.Death_day
+            ? { Deathday: Movie.Director.Death_day }
+            : { Deathday: "" };
+          let directorBirthday = Movie.Director.Birthday
+            ? { Birthday: Movie.Director.Birthday }
+            : { Deathday: "" };
+          return {
+            Genre: {
+              Description: Movie.Genre.Description,
+              Name: Movie.Genre.Name,
+            },
+            Director: {
+              Bio: Movie.Director.Bio,
+              Birthday: directorBirthday,
+              Deathday: directorDeathday,
+              Name: Movie.Director.Name,
+            },
+            _id: Movie._id,
+            Actors: Movie.Actors,
+            Title: Movie.Title,
+            Description: Movie.Description,
+            ImagePath: Movie.ImagePath,
+            Title: Movie.Title,
+          };
+        });
+        setMovies(moviesFromAPI);
+      })
+      .catch((error) => {
+        console.error(error);
+        // Handle the error here, e.g. display an error message
       });
-      setMovies(moviesFromAPI);
-    });
   }, []);
-
 
   if (selectedMovie) {
     return (
       <MovieView
-        movie={selectedMovie}
+        Movie={selectedMovie}
         onBackClick={() => setSelectedMovie(null)}
       />
     );
   }
-  if (movies.length === 0) {
+  if (Movies.length === 0) {
     return <div>The list is empty.</div>;
   }
   return (
     <div>
-      {movies.map((movie) => (
+      {Movies.map((Movie) => (
         <MovieCard
-          key={movie._id}
-          movie={movie}
+          key={Movie._id}
+          Movie={Movie}
           onMovieClick={(newSelectedMovie) => {
             setSelectedMovie(newSelectedMovie);
           }}
