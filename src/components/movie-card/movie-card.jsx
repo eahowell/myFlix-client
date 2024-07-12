39bc2019cd712e323173be8ea484b72418afe9da
Liz <51245710+eahowell@users.noreply.github.com>
2024-07-12 17:01:39 -0500

    Set Up BS Card for Movies

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,15 +1,19 @@
 import PropTypes from "prop-types";
+import "./movie-card.scss";
+import { Button, Card } from "react-bootstrap";
 
 export const MovieCard = ({ Movie, onMovieClick }) => {
-  return (
-    <div
-      className="movie-card"
-      onClick={() => {
-        onMovieClick(Movie);
-      }}
-    >
-      {Movie.Title}
-    </div>
+  return (    
+    <Card style = {{width: '15rem', cursor: "pointer"}} onClick={() => onMovieClick(Movie)} className="card">
+      <Card.Img variant="top" src={Movie.ImagePath} className="imgCard" style = {{width: '13.5rem'}}/>
+      <Card.Body>
+        <Card.Title className="cardTitle">{Movie.Title}</Card.Title>
+        <Card.Text>{Movie.Director.Name}</Card.Text>
+        <Button onClick={() => onMovieClick(Movie)} variant="primary">
+          More Details
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
